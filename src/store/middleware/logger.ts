94d299517b0dc28ae3cc59d928ec8e56fcecb8e3
jayoohy <ogukahjoy@gmail.com>
2025-07-30 21:48:50 +0100

    feat: migrate user and category sagas to TypeScript, add logger middleware, and update package dependencies

--- src/store/middleware/logger.ts
+++ src/store/middleware/logger.ts
@@ -2,7 +2,7 @@ import { Middleware } from "redux";
 import { RootState } from "../store";
 
 export const loggerMiddleware: Middleware<{}, RootState> =
-  (store) => (next) => (action) => {
+  (store) => (next) => (action: any) => {
     if (!action.type) {
       return next(action);
     }
